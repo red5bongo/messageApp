1cc8ff9a2233cfdb1eba0715671acccf933f9f74
Wil Seely-Kirk <wil.seely-kirk@sap.com>
2017-04-13 13:21:46 -0700

    First Commit

---
+++ api/controllers/Message/IDController.js
@@ -0,0 +1,11 @@
+/**
+ * Message/IDController
+ *
+ * @description :: Server-side logic for managing message/ids
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
